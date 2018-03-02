a8e38cfe0bc7a8e7a40d7428b87985c8e3de817b
Austin Bingham <austin.bingham@gmail.com>
2018-03-02 10:11:09 +0100

    Split image into "dependencies" and "source code".
    The "deps" image is slow to build but doesn't change very often. The "source"
    image builds quickly and changes a lot.

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -2,4 +2,5 @@
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
+docker build -t cyberdojo/runner_repl_base -f ${ROOT_DIR}/server/Dockerfile.base ${ROOT_DIR}/server
 docker-compose --file ${ROOT_DIR}/docker-compose.yml build
