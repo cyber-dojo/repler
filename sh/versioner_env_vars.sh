d9938be12c488b08e7af85f8ff79a27e3810f833
Jon Jagger <jon@jaggersoft.com>
2020-04-13 13:32:09 +0100

    Remove fake env-var now versioner embeds repler's env-vars

--- sh/versioner_env_vars.sh
+++ sh/versioner_env_vars.sh
@@ -3,7 +3,6 @@
 versioner_env_vars()
 {
   docker run --rm cyberdojo/versioner:latest
-  echo CYBER_DOJO_REPLER_IMAGE=cyberdojo/repler
   echo CYBER_DOJO_REPLER_CLIENT_PORT=9999
   echo CYBER_DOJO_REPLER_CLIENT_USER=nobody
   echo CYBER_DOJO_REPLER_SERVER_USER=root
