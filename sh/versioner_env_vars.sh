642075a6edf1ba483c05635caba084337d5a7f42
Jon Jagger <jon@jaggersoft.com>
2021-11-02 19:12:12 +0000

    Add merkely logging to CI pipeline.

--- sh/versioner_env_vars.sh
+++ sh/versioner_env_vars.sh
@@ -2,7 +2,12 @@
 
 versioner_env_vars()
 {
+  local -r sha="$(cd "${ROOT_DIR}" && git rev-parse HEAD)"
   docker run --rm cyberdojo/versioner:latest
+
+  echo CYBER_DOJO_REPLER_SHA="${sha}"
+  echo CYBER_DOJO_REPLER_TAG="${sha:0:7}"
+
   echo CYBER_DOJO_REPLER_CLIENT_PORT=9999
   echo CYBER_DOJO_REPLER_CLIENT_USER=nobody
   echo CYBER_DOJO_REPLER_SERVER_USER=root
