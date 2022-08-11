f7c083ff2c172d8fab0290abe13405a68ee83355
zhelezovartem <Zhelezovartem@gmail.com>
2022-08-11 12:34:43 +0300

    tf fmt

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "service_name" {
-  type = string
+  type    = string
   default = "repler"
 }
 
@@ -8,22 +8,22 @@ variable "env" {
 }
 
 variable "app_port" {
-  type = number
+  type    = number
   default = 4657
 }
 
 variable "cpu_limit" {
-  type = number
+  type    = number
   default = 20
 }
 
 variable "mem_limit" {
-  type = number
+  type    = number
   default = 64
 }
 
 variable "mem_reservation" {
-  type = number
+  type    = number
   default = 32
 }
 
